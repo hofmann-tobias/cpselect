d2e1f2491cefe6bd2b925a61555ed06ee51c1b83
Tobias <hofmann.tobias121gmail.com>
2019-04-02 18:11:48 +0200

    create package files

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="phg-cpselect-adal02",
+    version="0.0.1",
+    author="adal02r",
+    author_email="adal02.py@gmail.com",
+    description="Does what cpselect does in MATLAB.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adal02/phg-cpselect",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
