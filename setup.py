b1e14fd7c13d4ac2e88b8d7d69431f9f13dc4bc3
Tobias <hofmann.tobias121gmail.com>
2019-04-08 18:30:33 +0200

    prepared V1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cpselect",
-    version="1.0.0",
+    version="1.0.2",
     author="adal02",
     author_email="adal02.py@gmail.com",
     description="Does what cpselect does in MATLAB.",
